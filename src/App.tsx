3273ed0988f04715ec59ecd2164c7412062296f5
Paulo Morales <paulomorales@Paulos-MacBook-Air.local>
2024-12-03 00:46:23 -0300

    Reorganizar estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { VideoPlayer } from './components/VideoPlayer/VideoPlayer';
+import { VideoPlayer } from './components/VideoPlayer/VideoPlayer'
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
-      <VideoPlayer />
+    <div className="min-h-screen bg-zinc-950 text-zinc-50 flex items-center justify-center p-4">
+      <div className="w-full max-w-[1080px] aspect-video">
+        <VideoPlayer videoId="74efece0-263e-4307-ba14-85df445e55d5" />
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { VideoPlayer } from './components/VideoPlayer/VideoPlayer'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="min-h-screen bg-zinc-950 text-zinc-50 flex items-center justify-center p-4">
-      <div className="w-full max-w-[1080px] aspect-video">
-        <VideoPlayer videoId="74efece0-263e-4307-ba14-85df445e55d5" />
-      </div>
-    </div>
+    <App />
   </React.StrictMode>,
 )
