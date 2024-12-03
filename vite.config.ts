ed2aa0e5b0345233b3447544902dec7ff06c4a52
Paulo Morales <paulomorales@Paulos-MacBook-Air.local>
2024-12-03 00:48:08 -0300

    Atualizar configuração de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/player-aicrusacademy/',
+  base: './',
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
+    outDir: 'dist'
   }
 });
