960a2dafb13d70e0ea64b487bf61f3f98e275883
Paulo Morales <paulomorales@Paulos-MacBook-Air.local>
2024-12-03 01:00:23 -0300

    Simplificar toda a configuração

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/player-aicrusacademy/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        app: './index.html'
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
+  base: './'
 });
