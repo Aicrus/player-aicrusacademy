0045a780f492af98244092620ca58ef598ae8a07
Paulo Morales <paulomorales@Paulos-MacBook-Air.local>
2024-12-03 00:52:20 -0300

    Corrigir configuração de caminhos e deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/player-aicrusacademy/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 });
